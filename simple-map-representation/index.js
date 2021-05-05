151ed6e5b7a8757013e4034605edf752eedcf448
OzgeD <OzgeD@users.noreply.github.com>
2021-05-05 03:23:10 +0300

    creating our map

--- simple-map-representation/index.js
+++ simple-map-representation/index.js
@@ -1,9 +1,11 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+//latitude: 37.8921, longtitude: 32.4975, zoom:15
+var map = L.map('map').setView([37.8921, 32.4975], 15);
 
-var baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var baseMap = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    attribution:"Creating Map",
+    maxZoom:20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
 
-var marker = L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+baseMap.addTo(map);
+
